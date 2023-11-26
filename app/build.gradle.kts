c2a9f42bd7b3aa4aad57ced5f4fd11112a00b607
Nuray <210103331@stu.sdu.edu.kz>
2023-11-27 01:54:09 +0600

    1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,6 +78,9 @@ dependencies {
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+
+    implementation("com.squareup.retrofit2:converter-scalars:2.9.0")
 
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     debugImplementation("androidx.compose.ui:ui-tooling")
